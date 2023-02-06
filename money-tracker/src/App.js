87d52168aac0ad96ffd582e3db88afb21b9edcb7
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-02-07 00:01:59 +0530

    change to useeffect dependency using price

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -6,19 +6,21 @@ function App() {
   const [datetime, setDatetime] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
-  const [transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState([]);
 
+  useEffect(
+    () => {
+      getTransactions().then(transactions => {
+        setTransactions(transactions);
+      });
+    },
+    [price]
+  );
 
-  useEffect(()=>{
-    getTransactions().then(transactions => {
-      setTransactions(transactions);
-    })
-  },[])
-
-  async function getTransactions(){
+  async function getTransactions() {
     const url = process.env.REACT_APP_API_URL + "/transactions"; // getting from mongo
-    const res = await fetch(url) // get is default for fetch
-    return await res.json();    
+    const res = await fetch(url); // get is default for fetch
+    return await res.json();
   }
 
   function addNewTransaction(e) {
@@ -28,80 +30,103 @@ function App() {
     fetch(url, {
       method: "POST",
       headers: { "content-type": "application/json" },
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         price,
-        name, 
-        description, 
+        name,
+        description,
         datetime
       })
     }).then(res => {
       res.json().then(json => {
-        setName('');
-        setDatetime('');
-        setDescription('')
-        setPrice('')
+        setName("");
+        setDatetime("");
+        setDescription("");
+        setPrice("");
         console.log("result", json);
       });
     });
   }
 
   let balance = 0;
-  for(const transaction of transactions){
+  for (const transaction of transactions) {
     balance = balance + transaction.price;
   }
-  balance = balance.toFixed(2)
+  balance = balance.toFixed(2);
 
   return (
     <main>
-      <h1>₹{balance}</h1>
-      <form onSubmit={addNewTransaction}>
-        <div className="basic">
-          <input
-            type="text"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            placeholder="Example: Dinner"
-          />
-          <input
-            type="number"
-            value={price}
-            onChange={e => setPrice(e.target.value)}
-            placeholder="Example: -200" required
-          />
-        </div>
-        <div className="description">
-          <input
-            type="text"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-            placeholder="Note"
-          />
-          <input
-            type="datetime-local"
-            value={datetime}
-            onChange={e => setDatetime(e.target.value)}
-            placeholder="Date"
-          />
-        </div>
-        <button type="submit">Add new transaction</button>
-      </form>
-
-      <div className="transactions">
-        {transactions.length > 0 && transactions.map(transaction => (
-          <div className="transaction">
-          <div className="left">
-            <div className="name"> {transaction.name}</div>
-            <div className="description"> {transaction.description}</div>
+      <div className="fixed">
+        <h1>
+          ₹{balance}
+        </h1>
+        <form onSubmit={addNewTransaction}>
+          <div className="basic">
+            <input
+              type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder="Example: Dinner" required
+            />
+            <input
+              type="number"
+              value={price}
+              onChange={e => setPrice(e.target.value)}
+              placeholder="Example: -200"
+              required
+            />
           </div>
-          <div className="right">
-            <div className={"price " + (transaction.price<0?'red':'green')}>{transaction.price}</div>
-            <div className="datetime">{transaction.datetime}</div>
+          <div className="description">
+            <input
+              type="text"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              placeholder="Note" required
+            />
+            <input
+              type="datetime-local"
+              value={datetime}
+              onChange={e => setDatetime(e.target.value)}
+              placeholder="Date" required
+            />
           </div>
-        </div>
-        ))}
-        
+          <button type="submit">Add new transaction</button>
+        </form>
       </div>
-
+      <div className="transactions">
+        {transactions.length > 0 &&
+          transactions.map(transaction =>
+            <div className="transaction">
+              <div className="left">
+                <div className="name">
+                  {" "}{transaction.name}
+                </div>
+                <div className="description">
+                  {" "}{transaction.description}
+                </div>
+              </div>
+              <div className="right">
+                <div
+                  className={
+                    "price " + (transaction.price < 0 ? "red" : "green")
+                  }
+                >
+                  {transaction.price > 0
+                    ? "+" + transaction.price
+                    : transaction.price}
+                </div>
+                <div className="datetime">
+                  {transaction.datetime.split("T")[0]}{" "}
+                  <span className="time">
+                    {transaction.datetime
+                      .split("T")[1]
+                      .split(".")[0]
+                      .slice(0, -3)}
+                  </span>
+                </div>
+              </div>
+            </div>
+          )}
+      </div>
     </main>
   );
 }
